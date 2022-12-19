853e1de5b3affcab473e54b6de144731c3632c7e
Yuri Volegov <y.volegov@icloud.com>
2022-12-19 15:55:52 +0500

    Constrains added to place StackOfThreeViews in center with scaling views + IBOutlets added

--- SwiftBook/ViewController.swift
+++ SwiftBook/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redCircleView: UIView!
+    @IBOutlet var yellowCircleView: UIView!
+    @IBOutlet var greenCircleView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        print("viewDidLoad:", redCircleView.frame.width / 2)
     }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
 
+        redCircleView.layer.cornerRadius = redCircleView.frame.width / 2
+        yellowCircleView.layer.cornerRadius = yellowCircleView.frame.width / 2
+        greenCircleView.layer.cornerRadius = greenCircleView.frame.width / 2
+
+        print("viewWillLayoutSubviews:", redCircleView.frame.width / 2)
+    }
 
 }
 
