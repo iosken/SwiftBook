fc46c74dd04a92765eadd467f050038c567665a5
Yuri Volegov <y.volegov@icloud.com>
2022-12-21 14:42:29 +0500

    Hide and viewobjects by change Alpha channel in viewDidLoad(a-0) and viewDidAppear(a-0.3 - start position of hometask 2.2)

--- SwiftBook/ViewController.swift
+++ SwiftBook/ViewController.swift
@@ -8,22 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var redCircleView: UIView!
     @IBOutlet var yellowCircleView: UIView!
     @IBOutlet var greenCircleView: UIView!
-    
+    @IBOutlet var startNextColorLightButton: UIButton!
+       
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
         
+        /// hide objects to sneak transform:
+        redCircleView.alpha = 0
+        yellowCircleView.alpha = 0
+        greenCircleView.alpha = 0
+    }
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-
+        
         redCircleView.layer.cornerRadius = redCircleView.frame.width / 2
         yellowCircleView.layer.cornerRadius = yellowCircleView.frame.width / 2
         greenCircleView.layer.cornerRadius = greenCircleView.frame.width / 2
+        
+        ///immediately show objects after transformation(with hometask start a-level):
+        redCircleView.alpha = 0.3
+        yellowCircleView.alpha = 0.3
+        greenCircleView.alpha = 0.3
     }
-
+    
+    @IBAction func startNextColorLightButtonTapped() {
+        if redCircleView.alpha < 1 && yellowCircleView.alpha < 1 && greenCircleView.alpha < 1 {
+            startNextColorLightButton.setTitle("Next", for: .normal)
+        }
+    }
+    
+    
 }
-
