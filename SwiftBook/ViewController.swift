12a3ddfb59de657581ce3905cc6b754bc798cae4
Yuri Volegov <y.volegov@icloud.com>
2023-01-04 11:00:10 +0500

    All objects named + some outlets added

--- SwiftBook/ViewController.swift
+++ SwiftBook/ViewController.swift
@@ -9,8 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet var rgbView: UIView!
+    @IBOutlet var redHorisontalSlider: UISlider!
+    @IBOutlet var greenHorisontalSlider: UISlider!
+    @IBOutlet var blueHorisontalSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    func rgbViewSettingUp() {
+        rgbView.layer.cornerRadius = 16
+        rgbView.backgroundColor = UIColor.init(red: <#T##CGFloat#>, green: <#T##CGFloat#>, blue: <#T##CGFloat#>, alpha: <#T##CGFloat#>)
+    }
+    
+    func slidersSettingUp() {
+        
+    }
 }
+
+
 
