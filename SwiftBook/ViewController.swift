be7f9f66fe8d6eec046838a773d84baff2bff482
Yuri Volegov <y.volegov@icloud.com>
2023-01-11 15:49:41 +0500

    Autlets added

--- SwiftBook/ViewController.swift
+++ SwiftBook/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var randomNumberLabel: UILabel!
+    @IBOutlet var minValueLabel: UILabel!
+    @IBOutlet var maxValueLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func getResultButtonTaped(_ sender: UIButton) {
+        let minNumber = Int(minValueLabel.text ?? "") ?? 0
+        let maxNumber = Int(maxValueLabel.text ?? "") ?? 100
+        randomNumberLabel.text = Int.random(in: minNumber...maxNumber).formatted()
+    }
+    
 }
 
