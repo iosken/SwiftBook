9df56eaf595377ca1cd37e877a8c6b07fd82a63b
Yuri Volegov <y.volegov@icloud.com>
2023-01-13 07:52:31 +0500

    Prepare model

---
+++ SwiftBook/Model/Welcome.swift
@@ -0,0 +1,27 @@
+//
+//  WelcomeModel.swift
+//  SwiftBook
+//
+//  Created by Yuri on 12.01.2023.
+//
+
+struct Person {
+    ///name
+    ///about
+    ///hobbi
+    ///helth
+    ///resume
+    ///username
+    ///password
+    var name: String
+    var accessRights: Account
+    
+    
+}
+
+struct Account {
+    var userName: String
+    var password: String
+    
+    
+}
