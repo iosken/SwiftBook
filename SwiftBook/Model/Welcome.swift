1f90f4577a740732a6b87a72a8e53d4495caf1be
Yuri Volegov <y.volegov@icloud.com>
2023-01-20 08:25:22 +0500

    SingUP and SignIn added with userAccounts struct

--- SwiftBook/Model/Welcome.swift
+++ SwiftBook/Model/Welcome.swift
@@ -5,13 +5,15 @@
 //  Created by Yuri on 12.01.2023.
 //
 
-var autorizationBase: [String: String] = [:]
+struct UsersAccounts {
+    private var users: [String: User] = [:]
+}
 
 struct User {
-    var userName: String
-    var password: String
+    let login: String
+    let password: String
     
-    var person: Person
+    let person: Person
 }
 
 struct Person {
@@ -21,6 +23,36 @@ struct Person {
     var about: String?
     var hobbie: String?
     var helth: String?
+}
+
+extension UsersAccounts {
+    mutating func signUp(
+        login: String,
+        password: String,
+        firstName: String,
+        secondName: String,
+        about: String?,
+        hobbie: String?,
+        helth: String?
+    ) {
+        users = [
+            login + password : User(
+                login: login,
+                password: password,
+                person: Person(
+                    firstName: firstName,
+                    secondName: secondName,
+                    about: about,
+                    hobbie: hobbie,
+                    helth: helth
+                )
+            )
+        ]
+    }
+    
+    func signIn(login: String, password: String) -> User? {
+        users.keys.contains(login + password) ? users[login + password] : nil
+    }
 }
 
 
