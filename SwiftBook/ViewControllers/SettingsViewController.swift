419996a173c25845c58bd9655244b2202d974f94
Yuri Volegov <y.volegov@icloud.com>
2023-02-09 09:49:51 +0500

    Prepare fo hometask 2.9.1 Coloriset app with settings

--- SwiftBook/ViewControllers/SettingsViewController.swift
+++ SwiftBook/ViewControllers/SettingsViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SettingsViewController: UIViewController {
     // MARK: - IB Outlets
     @IBOutlet var rgbView: UIView!
     
---
+++ SwiftBook/ViewControllers/StartViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StartViewController.swift
+//  SwiftBook
+//
+//  Created by Yuri on 09.02.2023.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
