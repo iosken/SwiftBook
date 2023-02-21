7d853678947afaea733212daf8109abc0d65dfe0
Yuri Volegov(TEXHA) <y.volegov@icloud.com>
2023-02-21 08:14:56 +0500

    Storyboard TableViewController added

--- SwiftBook/ViewController.swift
+++ SwiftBook/ViewController.swift
@@ -17,3 +17,17 @@ class ViewController: UIViewController {
 
 }
 
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Cell index: \(indexPath.row)"
+        return cell
+    }
+    
+    
+}
