70fd8932310aeed476a63dd69516b194f0d13e36
Yuri Volegov(iosken) <y.volegov@icloud.com>
2023-05-04 14:32:37 +0500

    Spring animation dev start

--- SwiftBook/Extension + UIButton.swift
+++ SwiftBook/Extension + UIButton.swift
@@ -9,5 +9,16 @@ import Foundation
 import UIKit
 
 extension UIButton {
-    
+    func pulsate() {
+        let pulse = CASpringAnimation(keyPath: "transform.scale")
+        pulse.fromValue = 0.95 // стартовый размер
+        pulse.toValue = 1 // 100% конечный размер
+        pulse.duration = 0.6 // время анимации
+        pulse.autoreverses = true // маятник в другую сторону на основе старта-окончания
+        pulse.repeatCount = 2 // количество повторений
+        pulse.initialVelocity = 0.5 // начальное ускорение
+        pulse.damping = 1 // время остановки анимации
+        
+        layer.add(pulse, forKey:  nil)
+    }
 }
--- SwiftBook/ViewController.swift
+++ SwiftBook/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet var coreAnimationView: UIView!
+    
+    private var animationStarted = false
+
+    @IBAction func startCoreAnimation(_ sender: UIButton) {
+        sender.pulsate()
+        
+        UIView.animate(
+            withDuration: 0.5,
+            delay: 0,
+            options: [.autoreverse, .repeat]) { [unowned self] in
+                if !animationStarted {
+                    coreAnimationView.frame.origin.x -= 40
+                    animationStarted.toggle()
+                }
+            
+        }
     }
-
-
+    
 }
 
