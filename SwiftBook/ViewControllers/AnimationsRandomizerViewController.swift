16f6d9e1cdd804bbf09df3051eb73e5c00207656
Yuri Volegov(iosken) <y.volegov@icloud.com>
2023-05-08 21:54:46 +0500

    Program with random animations done

--- SwiftBook/ViewControllers/AnimationsRandomizerViewController.swift
+++ SwiftBook/ViewControllers/AnimationsRandomizerViewController.swift
@@ -6,24 +6,77 @@
 //
 
 import UIKit
+import SpringAnimation
 
 class AnimationsRandomizerViewController: UIViewController {
-
+    
+    @IBOutlet var presetLabel: UILabel!
+    @IBOutlet var curveLabel: UILabel!
+    @IBOutlet var forceLabel: UILabel!
+    @IBOutlet var durationLabel: UILabel!
+    @IBOutlet var delayLabel: UILabel!
+    
+    @IBOutlet var targetAnimationView: SpringView!
+    
+    lazy var nextAnimation = randomPreset()
+    
+    var firstRun = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        animate()
+    }
 
-        // Do any additional setup after loading the view.
+    @IBAction func runPressed(_ sender: UIButton) {
+        animate(sender: sender)
+    }
+}
+
+extension AnimationsRandomizerViewController {
+    
+    func animate(sender: UIButton? = nil) {
+        
+        targetAnimationView.animation = nextAnimation.animation
+        targetAnimationView.curve = nextAnimation.curve
+        targetAnimationView.force = nextAnimation.force
+        targetAnimationView.duration = nextAnimation.duration
+        targetAnimationView.delay = nextAnimation.delay
+        
+        presetLabel.text = "preset: " + targetAnimationView.animation
+        curveLabel.text = "curve: " + targetAnimationView.curve
+        forceLabel.text = "force: " + targetAnimationView.force.formatted()
+        durationLabel.text = "duration: " + targetAnimationView.duration.formatted()
+        delayLabel.text = "delay: " + targetAnimationView.delay.formatted()
+        
+        if firstRun {
+            firstRun.toggle()
+        } else {
+            targetAnimationView.animate()
+            nextAnimation = randomPreset()
+            sender?.setTitle(nextAnimation.animation, for: .normal)
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func randomPreset() -> (animation: String, curve: String, force: CGFloat, duration: CGFloat, delay: CGFloat) {
+        let animations = ["pop", "slideLeft", "slideRight", "slideDown", "slideUp", "squeezeLeft", "squeezeRight", "squeezeDown", "squeezeUp", "fadeIn", "fadeOut", "fadeOutIn", "fadeInLeft", "fadeInRight", "fadeInDown", "fadeInUp", "zoomIn", "zoomOut", "fall", "shake", "flipX", "flipY", "morph", "squeeze", "flash", "wobble", "swing"]
+        
+        let curves = ["easeIn", "easeOut", "easeInOut", "linear", "spring", "easeInSine", "easeOutSine", "easeInOutSine", "easeInQuad", "easeOutQuad", "easeInOutQuad", "easeInCubic", "easeOutCubic", "easeInOutCubic", "easeInQuart", "easeOutQuart", "easeInOutQuart", "easeInQuint", "easeOutQuint", "easeInOutQuint", "easeInExpo", "easeOutExpo", "easeInOutExpo", "easeInCirc", "easeOutCirc", "easeInOutCirc", "easeInBack", "easeOutBack", "easeInOutBack"]
+        
+        var force: CGFloat {
+            CGFloat.random(in: 1...5)
+        }
+        
+        var duration: CGFloat {
+            CGFloat.random(in: 0.5...5)
+        }
+        
+        var delay: CGFloat {
+            CGFloat.random(in: 0...2)
+        }
+        
+        return (animation: animations.randomElement() ?? "", curve: curves.randomElement() ?? "", force: force, duration: duration, delay: delay)
     }
-    */
-
 }
+
+
