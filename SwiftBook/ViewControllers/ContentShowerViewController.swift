6160fe564d2c6c217ba4c1ebad2d3f1160594f7e
Yuri Volegov(iosken) <y.volegov@icloud.com>
2023-05-22 07:21:55 +0500

    Base prepare to show content

--- SwiftBook/ViewControllers/ContentShowerViewController.swift
+++ SwiftBook/ViewControllers/ContentShowerViewController.swift
@@ -2,28 +2,61 @@
 //  ContentShowerViewController.swift
 //  SwiftBook
 //
-//  Created by Yuri Volegov on 05.05.2023.
+//  Created by Yuri Volegov on 19.05.2023.
 //
 
 import UIKit
 
-class ContentShowerViewController: UIViewController {
+enum UserAction: String, CaseIterable {
+    case emojihub = "Show Emoji!"
+    case genderize = "Tell about Name"
+    case swapi = "Description of Planet"
+    case wallstreetbet = "Some info about wallstreetbet"
+}
+
+class ContentShowerViewController: UITableViewController {
+    
+    let userActions = UserAction.allCases
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.rowHeight = 100
 
-        // Do any additional setup after loading the view.
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return userActions.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+        var configuration = cell.defaultContentConfiguration()
+        
+        configuration.text = userActions[indexPath.row].rawValue
+        configuration.image = UIImage(named: "content")
+        
+        cell.contentConfiguration = configuration
+
+        return cell
     }
     
+    // MARK: - Table view delegate
+    
+//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        performSegue(withIdentifier: <#T##String#>, sender: <#T##Any?#>)
+//    }
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        guard let indexPath = tableView.indexPathForSelectedRow else { return }
+        
+        
     }
-    */
 
 }
---
+++ SwiftBook/ViewControllers/DescriptionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DescriptionViewController.swift
+//  SwiftBook
+//
+//  Created by Yuri Volegov on 19.05.2023.
+//
+
+import UIKit
+
+class DescriptionViewController: UIViewController {
+
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
