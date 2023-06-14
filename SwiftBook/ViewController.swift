0f01ccf92be0b230e0187fd8932c9100e3a17db2
Yuri Volegov(iosken) <y.volegov@icloud.com>
2023-06-14 08:15:14 +0500

    textfield and button added

--- SwiftBook/ViewController.swift
+++ SwiftBook/ViewController.swift
@@ -8,10 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let label = UILabel()
+    private let textField = UITextField()
+    private let button = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(label)
+        view.addSubview(textField)
+        view.addSubview(button)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Have no data"
+        
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.backgroundColor = .blue.withAlphaComponent(0.2)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .blue.withAlphaComponent(0.5)
+        button.setTitle("Save", for: .normal)
+        button.addTarget(self, action: #selector(buttonTaped), for: .touchUpInside)
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -100),
+            
+            textField.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 20),
+            textField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            textField.widthAnchor.constraint(equalToConstant: 300),
+            
+            button.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 20),
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.widthAnchor.constraint(equalToConstant: 100)
+            ])
+    }
+    
+    @IBAction func buttonTaped() {
+        if textField.hasText {
+            print("Data saved")
+            label.text = textField.text
+        }
     }
 
 
