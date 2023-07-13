612f62528c3946fae711cd656a212da41c219c51
Yuri Volegov(iosken) <y.volegov@icloud.com>
2023-07-13 13:47:16 +0500

    sliders uped

--- SwiftBook/ContentView.swift
+++ SwiftBook/ContentView.swift
@@ -8,19 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var redSliderValue = Double.random(in: 0...255)
+    @State private var greenSliderValue = Double.random(in: 0...255)
+    @State private var blueSliderValue = Double.random(in: 0...255)
+    private var color: Color {
+        Color(
+            red: redSliderValue/255,
+            green: greenSliderValue/255,
+            blue: blueSliderValue/255
+        )
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            VStack(spacing: 20) {
+                colorShape(
+                    red: $redSliderValue,
+                    green: $greenSliderValue,
+                    blue: $blueSliderValue
+                )
+                ColorSliderView(value: $redSliderValue, color: .red)
+                ColorSliderView(value: $greenSliderValue, color: .green)
+                ColorSliderView(value: $blueSliderValue, color: .blue)
+                Text("\(redSliderValue) \(greenSliderValue) \(blueSliderValue)")
+                Spacer(minLength: 350)
+            }
         }
         .padding()
     }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct colorShape: View {
+    @Binding var red: Double
+    @Binding var green: Double
+    @Binding var blue: Double
+    
+    var body: some View {
+        Rectangle()
+            .cornerRadius(20)
+            .foregroundColor(Color(
+                red: red/255,
+                green: green/255,
+                blue: blue/255
+            ))
+    }
+    
+}
+
+struct ColorSliderView: View {
+    @Binding var value: Double
+    let color: Color
+    
+    var body: some View {
+        HStack {
+            Text("0")
+            Slider(value: $value, in: 0...255, step: 1)
+                .tint(color)
+         
+            Text("255")
+        }
     }
 }
