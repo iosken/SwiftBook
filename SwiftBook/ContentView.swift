adb5ad1346fc1bb856e65c8fa13154f0552aa3ff
Yuri Volegov(iosken) <y.volegov@icloud.com>
2023-07-16 23:21:27 +0500

    Ready to HW show (need refactor and focus change)

--- SwiftBook/ContentView.swift
+++ SwiftBook/ContentView.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     
+    
     @State private var redSliderValue = Double.random(in: 0...255)
     @State private var greenSliderValue = Double.random(in: 0...255)
     @State private var blueSliderValue = Double.random(in: 0...255)
+    
+    private let size = (UIScreen.main.bounds).size.width / 3
+    
     private var color: Color {
         Color(
             red: redSliderValue/255,
@@ -21,25 +25,55 @@ struct ContentView: View {
     }
     
     var body: some View {
-        
-        VStack(spacing: 40) {
-            colorShape(
-                red: $redSliderValue,
-                green: $greenSliderValue,
-                blue: $blueSliderValue
-            )
-            .frame(height: 200)
+        NavigationView {
+            VStack() {
+                colorShape(
+                    red: $redSliderValue,
+                    green: $greenSliderValue,
+                    blue: $blueSliderValue
+                ).frame(height: size)
+                
+                KeyboardView {
+                    VStack() {
+                        ColorSliderView(value: $redSliderValue, color: .red)
+                        ColorSliderView(value: $greenSliderValue, color: .green)
+                        ColorSliderView(value: $blueSliderValue, color: .blue)
+                    }.padding()
+                } toolBar: {
+                    HStack() {
+                        Spacer()
+                        Button(action: {
+                            UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                        }, label: {
+                            Text("Done")
+                        })
+                    }.padding()
+                }
 
-            VStack(spacing: 10) {
-                ColorSliderView(value: $redSliderValue, color: .red)
-                ColorSliderView(value: $greenSliderValue, color: .green)
-                ColorSliderView(value: $blueSliderValue, color: .blue)
             }
-
-            Spacer()
+            .padding()
+            .navigationBarTitle("Colorized app", displayMode: .automatic)
         }
-        .padding()
     }
+    
+    //        VStack(spacing: 40) {
+    //            colorShape(
+    //                red: $redSliderValue,
+    //                green: $greenSliderValue,
+    //                blue: $blueSliderValue
+    //            )
+    //            .frame(height: 200)
+    //
+    //            VStack(spacing: 10) {
+    //                ColorSliderView(value: $redSliderValue, color: .red)
+    //                ColorSliderView(value: $greenSliderValue, color: .green)
+    //                ColorSliderView(value: $blueSliderValue, color: .blue)
+    //            }
+    //
+    //            Spacer()
+    //        }
+    //        .padding()
+    //    }
     
 }
 
@@ -70,23 +104,25 @@ struct ColorSliderView: View {
     @Binding var value: Double
     @State private var text = ""
     @State private var alertPresented = false
+    @FocusState private var focusItem: Bool
     
     let color: Color
     
     var body: some View {
         HStack {
-            Text("\(lround(value))").frame(width: 45)
+            Text("\(lround(value))").frame(width: 40)
             Slider(value: $value, in: 0...255, step: 1).tint(color)
             
             TextField("\(lround(value))", text: $text, onEditingChanged: checkColorValue)
-                .frame(width: 45)
-//                .keyboardType(.numberPad)
-            
-            
+                .frame(width: 40)
+                .keyboardType(.numberPad)
+                .focused($focusItem)
                 .alert ("Wrong Format", isPresented: $alertPresented) {
                     Text ("Set number of color")
                 }
-            
+        }
+        .onTapGesture{
+            focusItem = false
         }
         .onAppear {
             text = String(lround(value))
---
+++ SwiftBook/KeyboardView.swift
@@ -0,0 +1,90 @@
+//
+//  KeyboardView.swift
+//  SwiftBook
+//
+//  Created by Yuri Volegov on 16.07.2023.
+//
+
+import SwiftUI
+
+final class KeyboardResponder: ObservableObject {
+    
+    private var notificationCenter: NotificationCenter
+    @Published private(set) var currentHeight: CGFloat = 0
+
+    init(center: NotificationCenter = .default) {
+        notificationCenter = center
+        notificationCenter.addObserver(
+            self,
+            selector: #selector(keyBoardWillShow),
+            name: UIResponder.keyboardWillShowNotification, object: nil)
+        notificationCenter.addObserver(
+            self, selector: #selector(keyBoardWillHide),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil)
+    }
+
+    func dismiss() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+    
+    deinit {
+        notificationCenter.removeObserver(self)
+    }
+
+    @objc func keyBoardWillShow(notification: Notification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            currentHeight = keyboardSize.height
+        }
+    }
+
+    @objc func keyBoardWillHide(notification: Notification) {
+        currentHeight = 0
+    }
+}
+
+struct KeyboardView<Content, ToolBar> : View where Content: View, ToolBar: View {
+    @StateObject private var keyboard = KeyboardResponder()
+    let toolbarFrame = CGSize(width: UIScreen.main.bounds.width, height: 40)
+    var content: () -> Content
+    var toolBar: () -> ToolBar
+    
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            content()
+              // .padding(.bottom, (keyboard.currentHeight == 0) ? 0 : toolbarFrame.height) //
+            VStack {
+                 Spacer()
+                 toolBar()
+                    .frame(width: toolbarFrame.width, height: toolbarFrame.height)
+                    .background(Color.secondary)
+                    
+            }
+            .opacity((keyboard.currentHeight == 0) ? 0 : 1)
+            .animation(.easeOut, value: 1)
+        }
+        
+        //.padding(.bottom, 80) //keyboard.currentHeight
+        .edgesIgnoringSafeArea(.bottom)
+        .animation(.easeOut, value: 1)
+    }
+}
+
+
+//var body: some View {
+//    ZStack(alignment: .topLeading) {
+//        content()
+//            .padding(.bottom, (keyboard.currentHeight == 0) ? 0 : toolbarFrame.height)
+//        VStack {
+//             Spacer()
+//             toolBar()
+//                .frame(width: toolbarFrame.width, height: toolbarFrame.height)
+//                .background(Color.secondary)
+//        }.opacity((keyboard.currentHeight == 0) ? 0 : 1)
+//        .animation(.easeOut, value: 1)
+//
+//    }
+//    .padding(.bottom, keyboard.currentHeight)
+//    .edgesIgnoringSafeArea(.bottom)
+//    .animation(.easeOut, value: 1)
+//}
